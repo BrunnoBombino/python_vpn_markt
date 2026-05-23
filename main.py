dbe798eefe136bdb8c68f4e9878fe6e845bd4f09
BrunnoBombino <tosser88@icloud.com>
2026-05-23 18:34:46 +0300

    Change

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from server import VPN
 
 vpn = VPN()
 vpn.connect()
+vpn.connect()
 all_users = vpn.users()
 vpn.save_json_data(all_users, "users.json")
 vpn.add_user(username="brunno", remark="Limit", days=10)
